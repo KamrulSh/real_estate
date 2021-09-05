58e1e84c66fa7ec8ba4b0845dea5284eccd8a067
Kamrul Islam <kamrul.shahin@bjitgroup.com>
2021-09-05 23:38:52 +0600

    rename some column and implement some constrains

--- models/realEstate.py
+++ models/realEstate.py
@@ -7,56 +7,74 @@ class RealEstate(models.Model):
     _name = 'realestate.properties'
     _description = 'Here Real Estate information is stored.'
 
-    name = fields.Char(string='Title')
+    name = fields.Char(string='Title', required=True)
     description = fields.Text(string='Description')
+    photo = fields.Binary(string="Photo", attachment=True)
 
-    post_code = fields.Char(string='Post Code')
-    exp_price = fields.Integer(string='Expected price')
-    bedrooms = fields.Integer(string='Bedrooms', default=3)
+    post_code = fields.Char(string='Post Code', required=True)
+    expected_price = fields.Integer(string='Expected price', required=True)
+    bedrooms = fields.Integer(string='Bedrooms', default=3, required=True)
     facades = fields.Integer(string='Facades')
     garden = fields.Boolean()
-    garden_orient = fields.Selection([('north_south', 'North to south'),
-        ('east_west', 'East to west')], string='Garden orientation')
-    
-    available = fields.Date(string='Available from', default= fields.datetime.now(), copy=False)
+    garden_orient = fields.Selection([('north', 'North'), ('south', 'South'),
+        ('east', 'East'), ('west', 'West')], string='Garden orientation')
+    garden_area = fields.Float(string='Garden Area(sqm)')
+
+    available_from = fields.Date(string='Available from', default= fields.datetime.now(), copy=False)
     deadline = fields.Date(string='Deadline', inverse="_inverse_total")
     selling_price = fields.Float(string='Selling Price', readonly=True, copy=False, compute="_compute_total")
-    living_area = fields.Integer(string='Living area(sqm)')
+    living_area = fields.Float(string='Living area(sqm)', required=True, default=1000)
     garage = fields.Boolean()
-    garden_area = fields.Integer(string='Garden Area(sqm)')
     status = fields.Selection([("new", "New"), ("offer_received", "Offer Received"),
         ("offer_accepted", "Offer Accepted"), ("sold", "Sold"), ("canceled", "Canceled")], 
         copy=False, default="new")
+    active = fields.Boolean(default=True)
 
-    email_id = fields.Char(String="Email address")
+    email_id = fields.Char(String="Email address", required=True)
     total_area = fields.Float(compute="_compute_total", readonly=True)
 
     # implement sql constraints
     _sql_constraints = [
-        ('check_exp_price', 'CHECK(exp_price > 0)', 'The expected price must be strictly positive'),
-        ('check_living_area', 'CHECK(living_area > 0)', 'The living area must be positive'),
-        ('check_garden_area', 'CHECK(garden_area >= 0)', 'The garden area must be positive'),
-        ('check_unique_email', 'UNIQUE(email_id)',   "The Email must be unique")
+        ('check_expected_price', 'CHECK(expected_price >= 0)', 'The expected price must be positive and greater than 0'),
+        ('check_living_area', 'CHECK(living_area > 0)', 'The living area must be positive and greater than 0'),
+        ('check_unique_email', 'UNIQUE(email_id)', "The Email must be unique"),
     ]
     
     # implement python constraints
-    @api.constrains('exp_price')
+    @api.constrains('expected_price', 'selling_price', 'garden')
     def _check_sellings_price(self):
         for record in self:
-            if record.exp_price < record.selling_price*0.9 or record.exp_price > record.selling_price:
-                raise ValidationError(f"""The expected price cannot be lower than 90% or more then 100% of selling price. 
-                Please make sure your price is between {record.selling_price*0.9} and {record.selling_price}""")
+            if record.garden == False or record.garden == True:
+                if record.expected_price < record.selling_price*0.9 or record.expected_price > record.selling_price:
+                    raise ValidationError(f"""The expected price cannot be lower than 90% or more then 100% of selling price. 
+                    Please make sure your price is between {record.selling_price*0.9} and {record.selling_price}""")
+            # A bug:
+            # when I don't change record.garden from current state and if I change
+            # living_area or garden_area then new selling_price will be calculated.
+            # But if I keep previous expected_price it won't check validation
+            # for updated selling_price and can be able to save in the database
+
+    @api.constrains('garden', 'garden_area')         
+    def _check_garden_area(self):
+        for record in self:
+            if record.garden == True and record.garden_area <= 0:
+                raise ValidationError("The garden area must be greater than 0")
 
     # implement computed fields and inverse function
-    @api.depends("living_area","garden_area", "selling_price", "available")
+    @api.depends("living_area", "garden", "garden_area", "selling_price", "available_from")
     def _compute_total(self):
         for record in self:
-            record.total_area = record.garden_area + record.living_area
-            record.selling_price = record.total_area * 20
+            if record.garden == False:
+                record.garden_area = 0
+                record.total_area = record.living_area
+                record.selling_price = record.total_area * 20
+            else:
+                record.total_area = record.garden_area + record.living_area
+                record.selling_price = record.total_area * 25
 
     def _inverse_total(self):
         for record in self:
-            record.deadline = record.available + timedelta(days=10)
+            record.deadline = record.available_from + timedelta(days=10)
             
     # implement email validation regex
     @api.onchange('email_id')
